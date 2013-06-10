6a14ee6a933945db280c6cfa062013aa94e8357f
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2013-06-10 12:11:11 +0200

    Remove mailbox notification

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,3 @@ for conf in ~/.config/fish/conf.d/*.fish
     . $conf
 end
 
-if status --is-interactive; and test (wc -l /var/spool/mail/danilo | awk '{ print $1 }') != 1
-    set fish_greeting "You have new mail!"
-end
