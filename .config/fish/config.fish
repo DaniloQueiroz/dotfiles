10fbafa1d8dd7a09f1bc0a7c99cbe66670cf70a7
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2013-06-10 12:40:14 +0200

    Fix colors and gitignore

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,13 @@
 # load files from conf.d dir
+set -U fish_color_status red
+set -U fish_color_cwd_root red
+set -U fish_color_cwd green
+set -U fish_color_status red
+set -U fish_color_user -o green
+set -U fish_color_host -o cyan
+
 for conf in ~/.config/fish/conf.d/*.fish
     . $conf
 end
 
+set -e fish_greeting
