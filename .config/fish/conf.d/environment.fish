6a5bb1e41c780144e9047ca578c1033ff2506b4e
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2014-01-29 21:32:53 +0100

    Update gradle files

--- .config/fish/conf.d/environment.fish
+++ .config/fish/conf.d/environment.fish
@@ -3,4 +3,4 @@ set -U EDITOR vim
 # PYTHON no '.pyc' files
 set -Ux PYTHONDONTWRITEBYTECODE 'please_dont'
 # gradle setup
-set PATH $PATH ~/Programming/resources/gradle/gradle-current/bin/
+set PATH $PATH ~/Programming/resources/gradle/bin/
