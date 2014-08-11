dcd10a2e15c26ac36ca55e8792f1e31237392684
Danilo Penna Queiroz <danilo@chaordicsystems.com>
2014-08-11 14:42:27 -0300

    Changes to fish aliases
    Split aliases on several files
    add revert alias

---
+++ .config/fish/conf.d/dev_aliases.fish
@@ -0,0 +1,7 @@
+# development aliases
+alias tests './tests.fish'
+alias build './build.fish'
+alias check './check.fish'
+alias clean './clean.fish'
+alias run './run.fish'
+alias gradlew './gradlew'
