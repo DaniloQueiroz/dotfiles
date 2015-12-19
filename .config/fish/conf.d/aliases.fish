864eac6db03fbc0471c7914daff896874b63cca5
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2015-12-18 22:42:30 -0200

    Add alias to fix thinkpad keyboard layout

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -6,4 +6,4 @@ alias py 'python'
 alias taillog 'colortail -f -n0'
 alias less '/usr/bin/less -MQRSi'
 alias pyclean 'pyclean -v'
-
+alias fix-keyboard '/usr/bin/setxkbmap -model thinkpad60 -layout br'
