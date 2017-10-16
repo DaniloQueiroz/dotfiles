20cb8d69b2f01905931712f4ee891155a97e4d5d
Danilo Queiroz <danilo@eventmobi.com>
2017-10-16 10:24:31 -0400

    Remove old fish stuff

--- .config/fish/conf.d/pyenv.fish
+++
@@ -1,2 +0,0 @@
-status --is-interactive; and source (pyenv init -|psub)
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
--- .config/fish/functions/times.fish
+++
@@ -1,8 +0,0 @@
-function times --description "Show current time on Europe/Berlin"
-    set TZs 'Europe/Berlin' 'America/Sao_Paulo' 
-    for tz in $TZs
-        set -x TZ $tz 
-        echo -n "Current Time on $tz: "
-        date
-    end
-end
