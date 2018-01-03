20d7c1c52122f484888163dfb0382b2146344cfc
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2018-01-03 07:47:13 -0500

    i3 colors and bar config

--- .config/i3/status.py
+++ .config/i3/status.py
@@ -3,15 +3,6 @@ from i3pystatus.updates import pacman, yaourt
 
 status = Status(standalone=True)
 
-#status.register(
-#    "network",
-#    format_up="{essid}  {quality}%",
-#    format_down=" {interface}",
-#    interface="wlp3s0",
-#    dynamic_color=False,
-#    on_leftclick=["connman-gtk"]
-#)
-
 status.register(
     "clock",
     format="%b %-d %k:%M")
@@ -22,59 +13,25 @@ status.register(
     charging_color="#00ff00",
     critical_color="#ff0000",
     battery_ident="ALL",
-    format="⚡ {status} {percentage:.2f}% [{remaining:%E%hh:%Mm}]",
+    format="{percentage:.2f}% [{remaining:%E%hh:%Mm}]",
     alert=True,
     alert_percentage=7,
     status={
-        "DIS": "",
-        "CHR": "",
-        "FULL": "",
+        "DIS": "[-]",
+        "CHR": "[+]",
+        "FULL": "[=]",
     })
 
-# status.register(
-#     "battery",
-#     full_color="#FFFFFF",
-#     charging_color="#00ff00",
-#     critical_color="#ff0000",
-#     battery_ident="BAT0",
-#     format="⚡0 {status} {percentage:.2f}% [{remaining:%E%hh:%Mm}]",
-#     alert=True,
-#     alert_percentage=7,
-#     status={
-#         "DIS": "",
-#         "CHR": "",
-#         "FULL": "",
-#    })
-# status.register(
-#    "battery",
-#     full_color="#FFFFFF",
-#     charging_color="#00ff00",
-#     critical_color="#ff0000",
-#     battery_ident="BAT1",
-#     format="⚡1 {status} {percentage:.2f}% [{remaining:%E%hh:%Mm}]",
-#     status={
-#         "DIS": "",
-#         "CHR": "",
-#         "FULL": "",
-#     })
-
-# status.register(
-#     "temp",
-#     format="{temp:.0f}°C",)
-
-# status.register(
-#     "cpu_freq",
-#     format=" {avgg}GHz")
-
-status.register(
-    "cpu_usage",
-    format=" {usage:02}%")
 
 status.register(
     "mem",
     color="#FFFFFF",
     warn_color="#FFFFFF",
-    format=" {percent_used_mem}%")
+    format="mem {percent_used_mem}%")
+
+status.register(
+    "cpu_usage",
+    format="cpu {usage:02}%")
 
 # status.register(
 #     "disk",
@@ -89,7 +46,7 @@ status.register(
 status.register(
     "updates",
     color="#FFFFFF",
-    format=" {count}",
+    format="!{count}",
     backends=[pacman.Pacman(), yaourt.Yaourt()])
 
 status.run()
