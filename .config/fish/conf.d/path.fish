887334479eb3c41e074695cbfe78941af618c946
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2018-03-13 20:47:34 -0400

    Home pc

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,5 +1,9 @@
-for i in (find -H  ~/tools -maxdepth 3 -type d -name bin)
-    set PATH $i $PATH
-end
+# i3dm path
+set PATH ~/tools/i3dm $PATH
 
 set PATH "" $PATH  # HACK: avoid eating first directory
+# utils path
+set PATH ~/tools/utils $PATH
+
+# android path
+set PATH ~/tools/android/platform-tools/ ~/tools/android/tools/bin $PATH
--- .config/fish/functions/on_pwd_changed.fish
+++ .config/fish/functions/on_pwd_changed.fish
@@ -2,18 +2,11 @@ function on_pwd_changed --on-variable PWD
   # skip if its a command substitution
   status --is-command-substitution; and return
 
-  # remove previous folder from PATH
-  set -e PATH[1]
-
-  # Check .files
+  ## venv Auto activation
+  # Check .venv files
   if test -f '.venv'  # virtualenv loading
     set _venv (cat .venv)
     echo "loading virtualenv $_venv..."
-    pyenv activate $_venv
-  end
-
-  # add current dir to PATH
-  if test $PATH[1] != $PWD
-    set -x PATH $PWD $PATH
+    venv activate $_venv
   end
 end
