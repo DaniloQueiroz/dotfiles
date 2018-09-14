b8d2307d9903899b574d3733e718df94c2f9f410
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2018-09-14 15:40:04 -0400

    Remove old fish functions and add support for .source files

--- .config/fish/functions/on_pwd_changed.fish
+++ .config/fish/functions/on_pwd_changed.fish
@@ -2,11 +2,17 @@ function on_pwd_changed --on-variable PWD
   # skip if its a command substitution
   status --is-command-substitution; and return
 
-  ## venv Auto activation
-  # Check .venv files
+  ## venv auto activation
+  # check .venv files
   if test -f '.venv'  # virtualenv loading
     set _venv (cat .venv)
     echo "loading virtualenv $_venv..."
     venv activate $_venv
+  end
+
+  if test -f '.source'
+    set _src (cat .source)
+    echo "loading $_src..."
+    source $_src
   end
 end
