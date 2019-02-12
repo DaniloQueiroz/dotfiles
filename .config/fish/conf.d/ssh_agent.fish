ce0e82a9f24a1bba7e5e99ae86fe43635a607545
Danilo Penna Queiroz <dpenna.queiroz@gmail.com>
2019-02-12 10:23:45 -0500

    SSH agent/config

--- .config/fish/conf.d/ssh_agent.fish
+++ .config/fish/conf.d/ssh_agent.fish
@@ -4,6 +4,5 @@ pgrep -a ssh-agent > /dev/null
 if test $status -eq 1
     set -e SSH_AUTH_SOCK
     set -Ux SSH_AUTH_SOCK (ssh-agent | grep SSH_AUTH_SOCK| awk -F'[=;]' '{ print $2 }')
-    ssh-add /home/danilo/.ssh/id_rsa
     ssh-add -L
 end
