ec59edf2971fe1ace1d7efd7792c68a90e48208a
danilo queiroz <dpenna.queiroz@gmail.com>
2022-06-17 17:25:26 -0400

    change secondary monitor rotation

--- config/bspwm/autostart.sh
+++ config/bspwm/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xrandr --output HDMI1 --auto --output HDMI2 --left-of HDMI1 --rotate left --auto
+xrandr --output HDMI1 --auto --output HDMI2 --left-of HDMI1 --rotate right --auto
 
 pgrep -x sxhkd > /dev/null || sxhkd -c $HOME/.config/bspwm/sxhkdrc &
 pgrep -x polybar > /dev/null || polybar -c $HOME/.config/bspwm/polybar main &
