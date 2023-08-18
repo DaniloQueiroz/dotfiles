f28e37c550127ccafe75c5a1c7f537abb5b65da5
danilo queiroz <dpenna.queiroz@gmail.com>
2023-08-18 00:57:10 -0400

    Remove keybind for chat gpt and create alias

--- config/fish/conf.d/gpt.fish
+++ config/fish/conf.d/gpt.fish
@@ -2,5 +2,5 @@ if test -r ~/.local/share/fish/openai.key
   set -x OPENAI_API_KEY (cat ~/.local/share/fish/openai.key)
 
   bind --user \e\r shellgpt
-  bind --user \eg chatgpt
+  alias c 'chatgpt' 
 end
