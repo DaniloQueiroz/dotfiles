2805a20298bac65ce86af320cdb63b91f3713d03
danilo queiroz <dpenna.queiroz@gmail.com>
2023-12-02 16:09:19 -0500

    Remove old files

--- bin/screenshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TO_FILE=${1:-n}
-
-if [ "$TO_FILE" == "y" ]; then
-    maim -s ~/screenshot-$(date +%s).png
-else
-    maim -s | xclip -selection clipboard -t image/png
-fi
--- config/bspwm/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-xrandr --output HDMI1 --mode 3440x1440 --output HDMI2 --left-of HDMI1 --rotate left --auto
-
-pgrep -x sxhkd > /dev/null || sxhkd -c $HOME/.config/bspwm/sxhkdrc &
-pgrep -x xss-lock > /dev/null || xss-lock xsecurelock &
-nitrogen --set-scaled --random ~/.config/wallpapers/
-$HOME/.config/polybar/launch.sh
-$HOME/.config/picom/launch.sh
