bc4bb6707c46305d7e2e9fa969d07fe8dab10b9a
danilo queiroz <dpenna.queiroz@gmail.com>
2023-12-03 11:34:00 -0500

    Configure gpg_tty

--- config/fish/conf.d/environment.fish
+++ config/fish/conf.d/environment.fish
@@ -4,3 +4,6 @@ set -xg EDITOR vim
 set -xg PAGER '/usr/bin/less -MQRSi'
 # PYTHON no '.pyc' files
 set -xg PYTHONDONTWRITEBYTECODE 'please_dont'
+
+# to make gpg work with yubico
+set -xg GPG_TTY $tty
