afb1ca16a80be4285ec3a889b3a5fad645fca895
danilo queiroz <dpenna.queiroz@gmail.com>
2024-08-30 15:51:56 -0400

    Config update

--- config/fish/conf.d/abbr.fish
+++ config/fish/conf.d/abbr.fish
@@ -1,10 +1,17 @@
-abbr --add v 'vim'
-abbr --add g 'git'
-abbr --add b 'bat'
 abbr --add a 'asdf'
-abbr --add sys 'systemctl'
-abbr --add dk 'docker'
-abbr --add k 'kubectl'
-abbr --add t 'tailscale'
+abbr --add b 'bat'
+
 abbr --add cs 'cheatsheet'
+abbr --add dk 'docker'
+
+abbr --add g 'git'
+abbr --add k 'kubectl'
+
 abbr --add pf 'pass-fzf'
+abbr --add sys 'systemctl'
+
+abbr --add tk 'task'
+abbr --add tf 'terraform'
+abbr --add ts 'tailscale'
+
+abbr --add v 'vim'
